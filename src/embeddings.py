308394e9ce58527a1ee25e055e79ea758d1f556a
BODDEPALLI USHAKIRAN <131387978+USHAKIRAN1729@users.noreply.github.com>
2026-02-23 08:23:15 +0530

    Change model to paraphrase-MiniLM-L3-v2

--- src/embeddings.py
+++ src/embeddings.py
@@ -1,7 +1,7 @@
 from sentence_transformers import SentenceTransformer
 from sklearn.preprocessing import normalize
 
-model = SentenceTransformer("all-MiniLM-L6-v2")
+model = SentenceTransformer("paraphrase-MiniLM-L3-v2")
 
 def generate_embeddings(texts):
     emb = model.encode(texts, show_progress_bar=True)
